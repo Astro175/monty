4d7ff0aa088ce63e2ec50b16594b460fc7ea95f8
Astro175 <aspark400@gmail.com>
2023-01-19 22:00:12 +0100

    updated

--- main.c
+++ main.c
@@ -1,38 +1,38 @@
 #include "monty.h"
+#include <stdio.h>
 #define _GNU_SOURCE
-
+#include <stdlib.h>
 
 bus_t bus = {NULL, NULL, NULL, 0};
-/**
- * main - program that performs stack operations
- * @argv: array of arguments
- * @argc: number of arguments
- * Return: Always 0
- */
 
-int main(int argc, char **argv)
+/**
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
 {
 	char *content;
 	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
-	stack_t stack = NULL;
+	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: Can't open file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(agrv[1], "r");
+	file = fopen(argv[1], "r");
 	bus.file = file;
-
 	if (!file)
 	{
-		fprintf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (read_line > 0)
 	{
 		content = NULL;
@@ -41,11 +41,11 @@ int main(int argc, char **argv)
 		counter++;
 		if (read_line > 0)
 		{
-			execute(&content, &stack, counter, file);
+			execute(content, &stack, counter, file);
 		}
 		free(content);
 	}
 	free_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
