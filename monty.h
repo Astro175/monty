81188b14b2f83563890551e91684ece02e611e3f
Zigijos <zigijos@gmail.com>
2023-01-19 20:33:05 +0000

    commit

--- monty.h
+++ monty.h
@@ -54,4 +54,23 @@ typedef struct bus_s
 extern bus_t bus;
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
+void addnode(stack_t **head, int n);
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void f_add(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pop(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_push(stack_t **head, unsigned int counter);
+void f_queue(stack_t **head, unsigned int counter);
+void addqueue(stack_t **head, int n);
 #endif
